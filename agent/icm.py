824ed1d244a88e255e4cda64f629f09a5efb2e4e
Manfred Diaz <takeitallsource@gmail.com>
2022-03-22 17:27:09 -0400

    Noisy ICM

--- agent/icm.py
+++ agent/icm.py
@@ -79,13 +79,13 @@ class ICMAgent(DDPGAgent):
     def compute_intr_reward(self, obs, action, next_obs, step):
         forward_error, _ = self.icm(obs, action, next_obs)
 
-        stddev = utils.schedule(self.stddev_schedule, step)
-        dist = self.actor(obs, stddev)
-        log_prob = dist.log_prob(action)
+        # stddev = utils.schedule(self.stddev_schedule, step)
+        # dist = self.actor(obs, stddev)
+        # log_prob = dist.log_prob(action)
 
-        reward = forward_error - 0.05 * log_prob.sum(axis=-1, keepdim=True) #* self.icm_scale
+        # reward = forward_error - 0.05 * log_prob.sum(axis=-1, keepdim=True) #* self.icm_scale
         # reward = torch.log(reward + 1.0)
-        return reward
+        return forward_error.torch.fill_(5) + torch.randn_like(forward_error, device=self.device)
 
     def update(self, replay_iter, step):
         metrics = dict()
